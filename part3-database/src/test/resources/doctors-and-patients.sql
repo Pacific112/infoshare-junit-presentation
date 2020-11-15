2eef0e2c4800c898e5f3727f1af5d4b6e9db772a
maciej.koziara <maciej.koziara@airhelp.com>
2020-11-15 15:33:11 +0100

    Remove tests from presentation

--- part3-database/src/test/resources/doctors-and-patients.sql
+++
@@ -1,6 +0,0 @@
-insert into doctor values (1, 'Doctor Jacek');
-insert into doctor values (2, 'Doctor inny');
-
-insert into patient values (1, 28, '22222', 1);
-insert into patient values (2, 25, '111111', 1);
-insert into patient values (3, 25, 'asndjiasdask', 2);
